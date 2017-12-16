aae094f87975298d64196ee25db2adf7fa309508
Sanvenir <sanvenir@outlook.com>
2017-12-16 14:44:58 +0800

    Location getting complete

--- app/src/main/java/com/sanvenir/positiongetter/LocationService.java
+++ app/src/main/java/com/sanvenir/positiongetter/LocationService.java
@@ -1,16 +1,56 @@
 package com.sanvenir.positiongetter;
 
+import android.Manifest;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class LocationService extends Service {
     private static final String TAG = LocationService.class.getSimpleName();
+
+    private LocationManager locationManager;
+    private String locationProvider;
+    private Timer mTimer;
+
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            showLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 
     private final IBinder mBinder = new LocalBinder();
 
@@ -20,17 +60,58 @@ public class LocationService extends Service {
         }
     }
 
+    private void showLocation(Location location) {
+        Log.d(TAG, "===================>" + location);
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        List<String> providers = locationManager.getProviders(true);
+        if(providers.contains(LocationManager.GPS_PROVIDER)) {
+            locationProvider = LocationManager.GPS_PROVIDER;
+        } else if(providers.contains(LocationManager.NETWORK_PROVIDER)) {
+            locationProvider = LocationManager.NETWORK_PROVIDER;
+        } else {
+            Log.d(TAG, "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!No location provider available");
+            return;
+        }
+        Log.d(TAG, "==========================>location manager: " + locationManager);
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Log.d(TAG, "==========================>Location setup complete");
+        locationManager.requestLocationUpdates(locationProvider, 3000, 0.1f, locationListener);
 
+    }
+
+    public void startTimer() {
+        if(mTimer == null) {
+            mTimer = new Timer();
+        }
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Log.d("Timer Thread", "========================>Running");
+                if(ActivityCompat.checkSelfPermission(LocationService.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                Location location = locationManager.getLastKnownLocation(locationProvider);
+                if(location != null) {
+                    showLocation(location);
+                }
+            }
+        }, 1000, 5000);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Notification.Builder builder = new Notification.Builder(this.getApplicationContext());
+        Notification.Builder builder = new Notification.Builder(this);
         Intent nfIntent = new Intent(this, MapsActivity.class);
-        builder.setContentIntent(PendingIntent.getActivity(this, 0, nfIntent, 0))
+        builder.setContentIntent(PendingIntent.getActivity(this, 0, nfIntent, PendingIntent.FLAG_UPDATE_CURRENT))
                 .setLargeIcon(BitmapFactory.decodeResource(this.getResources(), R.mipmap.ic_launcher))
                 .setContentTitle("Geo Service")
                 .setSmallIcon(R.mipmap.ic_launcher)
@@ -38,8 +119,16 @@ public class LocationService extends Service {
                 .setWhen(System.currentTimeMillis());
         Notification notification = builder.build();
         startForeground(110, notification);
-
+        startTimer();
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    public class AlarmReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Intent i = new Intent(context, LocationService.class);
+            context.startService(i);
+        }
     }
 
     @Nullable
